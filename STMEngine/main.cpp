9c543d14fb3de6ade3ae858f05138b3830003a49
fuyingdi <fuyingdi@126.com>
2020-01-01 17:58:02 +0800

    init

--- STMEngine/main.cpp
+++ STMEngine/main.cpp
@@ -1,37 +1,9 @@
 ﻿#define OLC_PGE_APPLICATION
 #include "STMEngine.h"
 
-class Example : public STM::PixelGameEngine
-{
-public:
-	Example()
-	{
-		sAppName = "Example";
-	}
-
-public:
-	bool OnUserCreate() override
-	{
-		// Called once at the start, so create things here
-		return true;
-	}
-
-	bool OnUserUpdate(float fElapsedTime) override
-	{
-		// called once per frame
-		for (int x = 0; x < ScreenWidth(); x++)
-			for (int y = 0; y < ScreenHeight(); y++)
-				Draw(x, y, STM::Pixel(rand() % 255, rand() % 255, rand() % 255));
-		return true;
-	}
-};
 
 
 int main()
 {
-	Example demo;
-	if (demo.Construct(256, 240, 4, 4))
-		demo.Start();
-
-	return 0;
+	std::cout<<"ok";
 }
